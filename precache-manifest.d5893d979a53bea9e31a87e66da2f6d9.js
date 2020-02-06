525d11b77571d38c4ecc8ce04aa919938cb60315
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-06 15:24:34 +0200

    Init commit

---
+++ precache-manifest.d5893d979a53bea9e31a87e66da2f6d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e792dad5fe1d185b1f8df610d1e1ed90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d64f0b8065708190a61",
+    "url": "/static/css/main.d29db5d2.chunk.css"
+  },
+  {
+    "revision": "0b8790e139d64fa5e37c",
+    "url": "/static/js/2.1b5227b7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1b5227b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d64f0b8065708190a61",
+    "url": "/static/js/main.00edf3d8.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "42d894b5e604747c7640f250038d285b",
+    "url": "/static/media/face.42d894b5.jpg"
+  }
+]);
