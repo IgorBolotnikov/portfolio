ed70d48835b3a8f79dcb289b50bd6e0cfcf25a21
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-06 15:32:35 +0200

    Updates

---
+++ precache-manifest.b8b1b649510d18d59831293dbc0361eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9a5b5dde0d0f1f13493ced2d150f00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a27f5ea6e96bcc62d7f6",
+    "url": "/portfolio/static/css/main.d29db5d2.chunk.css"
+  },
+  {
+    "revision": "0b8790e139d64fa5e37c",
+    "url": "/portfolio/static/js/2.1b5227b7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.1b5227b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27f5ea6e96bcc62d7f6",
+    "url": "/portfolio/static/js/main.96b0b2fe.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "42d894b5e604747c7640f250038d285b",
+    "url": "/portfolio/static/media/face.42d894b5.jpg"
+  }
+]);
