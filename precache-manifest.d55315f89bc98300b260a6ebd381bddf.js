9cac40643aeb1fdc563a5754ac5b297ce2388bf5
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-07 10:10:47 +0200

    Updates

---
+++ precache-manifest.d55315f89bc98300b260a6ebd381bddf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010e535ef6d7b2c3ad7141fbefc6f106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d4bb99805e7b3c07a1",
+    "url": "/static/css/main.1e98a777.chunk.css"
+  },
+  {
+    "revision": "55c214198d9f49ec6c72",
+    "url": "/static/js/2.b9d95ac5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b9d95ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d4bb99805e7b3c07a1",
+    "url": "/static/js/main.59bebe2c.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "cf5ec9cb33d6884671440e57b35b994e",
+    "url": "/static/media/GitHub-logo-small.cf5ec9cb.png"
+  },
+  {
+    "revision": "167bf1c0bac2b8b3eb3158ae87390b7b",
+    "url": "/static/media/LinkedIn-logo-small.167bf1c0.png"
+  },
+  {
+    "revision": "42d894b5e604747c7640f250038d285b",
+    "url": "/static/media/face.42d894b5.jpg"
+  },
+  {
+    "revision": "39f7808b2edbfc3df129d8922cb7f412",
+    "url": "/static/media/karate_cat.39f7808b.gif"
+  },
+  {
+    "revision": "0fc3a681b52b96d23c59c74d84562705",
+    "url": "/static/media/scrpr.0fc3a681.png"
+  },
+  {
+    "revision": "475bf47f565e37d419baa0883d83e339",
+    "url": "/static/media/sudoku_solver.475bf47f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.11507fc6442f4db2ac0eb2d84605925f.js"
+  "/precache-manifest.d55315f89bc98300b260a6ebd381bddf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
